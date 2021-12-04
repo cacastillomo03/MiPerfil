259da9d866ea8976b18cd97ee9208fe04ea2a139
cacastillomo03 <castillomoran@hotmail.com>
2021-12-04 18:26:08 -0500

    se creo un menu de navegacion y se enlazaron los documentos html para  cada uno de los item

--- public_html/JS/menu.js
+++ public_html/JS/menu.js
@@ -16,9 +16,3 @@ window.addEventListener('click', e=>{
 });
 
 
-function menosQueNoventa(num) {
-  // Devuelve "true" si el argumento de la función "num" es menor que noventa
-  // De lo contrario, devuelve "false"
-  // Tu código:
-    return num<90; 
-}
